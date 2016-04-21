fe31e5ef81ae8229d1f34a3d9f0952e527828da8
Rohan Patel <rohanp4636@gmail.com>
2016-04-21 15:53:35 -0400

    replay xml

--- app/src/main/java/com/example/daivikrohan/chessapp01/ImageAdapter.java
+++ app/src/main/java/com/example/daivikrohan/chessapp01/ImageAdapter.java
@@ -1,7 +1,49 @@
 package com.example.daivikrohan.chessapp01;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.FrameLayout;
+
 /**
  *
  */
-public class ImageAdapter {
+public class ImageAdapter extends BaseAdapter {
+    FrameLayout frameLayout;
+    private Context mContext;
+    private LayoutInflater inflater;
+
+    //this array stores the squares for the chess board.
+    private Integer boardID[] ={};
+
+
+    public ImageAdapter(Context c) {
+        this.mContext = c;
+        this.frameLayout = null;
+        this.inflater = null;
+    }
+
+    public int getCount() {
+        return boardID.length;
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if(convertView == null){
+            convertView = inflater.inflate(R.layout.chesspiece,null);
+            BoardSquarePiece bsp = new BoardSquarePiece( position,boardID, convertView);
+            convertView.setTag(bsp);
+            return convertView;
+        }
+        return convertView;
+    }
 }
--- app/src/main/java/com/example/daivikrohan/chessapp01/MainActivity.java
+++ app/src/main/java/com/example/daivikrohan/chessapp01/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.daivikrohan.chessapp01;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button play = (Button) findViewById(R.id.PlayButton);
+        Button replay = (Button)findViewById(R.id.ReplayButton);
+        if(play != null) {
+            play.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    Intent i = new Intent(MainActivity.this, ChessActivity.class);
+                    startActivityForResult(i, 0);
+                }
+            });
+        }
+        /*
+        if(replay != null) {
+            play.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    Intent i = new Intent(MainActivity.this, ChessActivity.class);
+                    startActivityForResult(i, 0);
+                }
+            });
+        }*/
+
     }
+
 }
